39a5b98d8d863fd52af329cc5ddf4092438741e3
Pedro Sousa <pedrosousa2708@gmail.com>
2021-01-25 14:38:47 -0300

    Evaluator class now has a satifability function

--- src/main.py
+++ src/main.py
@@ -1,34 +1,33 @@
 from Formulas import *
-from Formatter import *
 from Evaluator import *
 
 def main():
-
-	definition = {
-					"p": True,
-					"q": False
-				}
-
-	evaluator = Evaluator(definition)
+	evaluator = Evaluator()
 
 	atomP = Atom("p")
 	atomQ = Atom("q")
 	atomR = Atom("r")
+	atomR = Atom("r")
 
-	formula1 = Not(atomR) 			 # ~r
-	formula2 = And(atomP, atomQ) 	 # p ^ q
-	formula3 = Or(atomP, atomR) 	 # p v r
-	formula4 = And(atomP, atomR) 	 # p ^ r
-	formula5 = Implies(atomP, atomR) # p -> r
-	formula6 = Implies(atomR, atomP) # r -> p
+	formula1 = Not(atomR) 			  # ~r
+	formula2 = And(atomP, atomQ) 	  # p ^ q
+	formula3 = Or(atomP, atomR) 	  # p v r
+	formula4 = Implies(atomP, atomR)  # p -> r
+	formula5 = And(atomP, Not(atomP)) # p ^ ~p
 
-	print(evaluator.truthValueOf(formula1))
-	print(evaluator.truthValueOf(formula2))
-	print(evaluator.truthValueOf(formula3))
-	print(evaluator.truthValueOf(formula4))
-	print(evaluator.truthValueOf(formula5))
-	print(evaluator.truthValueOf(formula6))
+	atomSatisfability = evaluator.satisfability(atomP, mode=Evaluator.satisfactory)
+	notSatisfability = evaluator.satisfability(formula1, mode=Evaluator.satisfactory)
+	andSatisfability = evaluator.satisfability(formula2, mode=Evaluator.satisfactory)
+	orDoubleSatisfactory = evaluator.satisfability(formula3, mode=Evaluator.doubleSatisfactory)
+	impliesAllModels = evaluator.satisfability(formula4, mode=Evaluator.allModels)
+	unsatisfactory = evaluator.satisfability(formula5, mode=Evaluator.satisfactory)
 
+	print(atomSatisfability)
+	print(notSatisfability)
+	print(andSatisfability)
+	print(orDoubleSatisfactory)
+	print(impliesAllModels)
+	print(unsatisfactory)
 
 
 if __name__ == "__main__":
